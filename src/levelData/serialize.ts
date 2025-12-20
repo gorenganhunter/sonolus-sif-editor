f255e31c9e2648277c6e49864653f8eee0ac8adf
gorenganhunter <gorengandev@gmail.com>
2025-12-20 18:45:46 +0700

    calculate life

--- src/levelData/serialize.ts
+++ src/levelData/serialize.ts
@@ -1,12 +1,26 @@
 import type { LevelData } from '@sonolus/core'
 import type { Store } from '../state/store'
 import { serializeToLevelDataEntities } from './entities/serialize'
+import { beatToTime } from '../state/integrals/bpms'
+import { bpms } from '../history/bpms'
 
 export const serializeToLevelData = (
     bgmOffset: number,
     store: Store,
     groupCount: number,
 ): LevelData => {
+    let firstBeat = Infinity
+    let lastBeat = -Infinity
+    let duration = 0
+    for (const note of store.grid.note.values()) {
+        note.forEach(n => {
+            if (n.beat < firstBeat) firstBeat = n.beat
+            if (n.beat > lastBeat) lastBeat = n.beat
+        })
+    }
+    if (firstBeat !== Infinity && lastBeat !== -Infinity) {
+        duration = beatToTime(bpms.value, lastBeat) - beatToTime(bpms.value, firstBeat)
+    }
     const data = {
         bgmOffset,
         entities: [
@@ -15,7 +29,7 @@ export const serializeToLevelData = (
                 data: [
                     {
                         name: 'life',
-                        value: 30
+                        value: Math.floor(duration / 6) + 1
                     }
                 ],
             },
